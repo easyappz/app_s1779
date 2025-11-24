af8d00b881246e314e3b809249270b044f608fd8
IbragimKhaniev <newinterny@gmail.com>
2025-11-24 10:18:57 +0300

    Add serializers for Member, Register, Login and Message

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,44 @@
 from rest_framework import serializers
+from api.models import Member, Message
 
 
-class MessageSerializer(serializers.Serializer):
-    message = serializers.CharField(max_length=200)
-    timestamp = serializers.DateTimeField(read_only=True)
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = ['id', 'username', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, max_length=128)
+
+    class Meta:
+        model = Member
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        member = Member(
+            username=validated_data['username']
+        )
+        member.set_password(validated_data['password'])
+        member.save()
+        return member
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True, max_length=150)
+    password = serializers.CharField(write_only=True, max_length=128)
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    author = MemberSerializer(read_only=True)
+    author_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'text', 'author', 'author_id', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        return representation
